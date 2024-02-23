546d4bd6f4b9d097cc952d4138619c95a1342d9d
Sterling <sktucker21@byui.edu>
2024-02-23 15:08:28 -0700

    wk07 more fixes to index.js and server.js personal

--- routes/index.js
+++ routes/index.js
@@ -11,26 +11,23 @@ const config = {
     baseURL: 'https://cse341-winter24-uqt1.onrender.com',
     clientID: 'Hff8lp9f8fLlCcAPpCktmmcEXRRNk5VP',
     issuerBaseURL: 'https://dev-aq4g00mxk5l1itvh.us.auth0.com'
-  };
+};
   
-  // auth router attaches /login, /logout, and /callback routes to the baseURL
-  app.use(auth(config));
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
   
-  // req.isAuthenticated is provided from the auth router
-  app.get('/', (req, res) => {
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
     res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-  });
+});
 
 routes.use('/', require('./swagger'));
 routes.use('/movies', movie);
 
-
 routes.use('/', (req, res, next) => {
-    
     req.docData = {
         documentationURL: 'https://github.com/sterlingt22/movie_manager.git',
     };
-   
     next();
 });
 
--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongodb = require('./db/connect')
+const mongodb = require('./db/connect');
 const port = process.env.PORT || 8080;
  
 const app = express();
 
 app
     .use(bodyParser.json())
-//set up CORS headers
+    // Set up CORS headers
     .use((req, res, next) => {
         res.setHeader('Access-Control-Allow-Origin', '*');
         next();
     })
     .use('/', require('./routes'));
- 
+
 process.on('uncaughtException', (err, origin) => {
     console.log|process.stderr.fd, `Caught exception: ${err}\n` + `Exception origin: ${origin}`;
 });
@@ -22,9 +22,10 @@ mongodb.initDb((err) => {
     if (err) {
         console.log(err);
     } else {
-        app.listen(port);
+        app.listen(port, () => {
             console.log(`Connected to database and listening on ${port}`);
-        }
+        });
+    }
 });
 
 module.exports = app;
