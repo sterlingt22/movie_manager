6c238f92383f1de99aa6fc2f6794617c9dab0864
Sterling <sktucker21@byui.edu>
2024-02-23 14:25:12 -0700

    New commit for personal project

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,23 @@
 const routes = require('express').Router();
 const movie = require('./movies');
+const { auth } = require('express-openid-connect');
+
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: 'a long, randomly-generated string stored in env',
+    baseURL: 'https://cse341-winter24-uqt1.onrender.com',
+    clientID: 'Hff8lp9f8fLlCcAPpCktmmcEXRRNk5VP',
+    issuerBaseURL: 'https://dev-aq4g00mxk5l1itvh.us.auth0.com'
+  };
+  
+  // auth router attaches /login, /logout, and /callback routes to the baseURL
+  app.use(auth(config));
+  
+  // req.isAuthenticated is provided from the auth router
+  app.get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+  });
 
 routes.use('/', require('./swagger'));
 routes.use('/movies', movie);
--- server.js
+++ server.js
@@ -1,30 +1,29 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const mongodb = require('./db/connect');
-const router = require('./routes'); 
+const app = express();
+const { auth, requiresAuth } = require('express-openid-connect');
+require('dotenv').config();
 
 const port = process.env.PORT || 3000;
-const app = express();
 
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+};
 
-app.use(bodyParser.json());
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  next();
-})
+app.use(auth(config));
 
-.use('/', require('./routes'));
-
-process.on('uncaughtException', (err, origin) => {
-  console.log(process.stderr.fd, `Caught exception: ${err}\n` + `Exception origin: ${origin}`);
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
 });
 
-mongodb.initDb((err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    app.listen(port, () => {
-      console.log(`Connected to DB and listening on ${port}`);
-    });
-  }
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
 });
