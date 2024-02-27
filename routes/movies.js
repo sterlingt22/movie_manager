41411e911acd3d322adfa097b67f6821f682e374
Sterling <sktucker21@byui.edu>
2024-02-26 18:05:05 -0700

    Trying to get rid of the 500 error

--- routes/movies.js
+++ routes/movies.js
@@ -1,24 +1,15 @@
 const express = require('express');
-const routes = express.Router(); // Change variable name from router to routes
 const mongodb = require('../db/connect');
 const { ObjectId } = require('mongodb');
 const moviesController = require('../controllers/movies');
 const validateMovie = require('../middleware/validate');
-const { auth } = require('express-openid-connect');
 
-const config = {
-    authRequired: false,
-    auth0Logout: true,
-    secret: '57xg29C13cCPU6zkthW0Q5Cg5PuUcWtNn_KkFymFeOXwolvRFNREgmCMp8oBqt5h',
-    baseURL: 'https://cse341-winter24-uqt1.onrender.com',
-    clientID: 'Hff8lp9f8fLlCcAPpCktmmcEXRRNk5VP',
-    issuerBaseURL: 'https://dev-aq4g00mxk5l1itvh.us.auth0.com'
-};
+const routes = express.Router();
 
-routes.get('/', auth(config), moviesController.getAll);
-routes.get('/:id', auth(config), moviesController.getSingle);
-routes.post('/', auth(config), validateMovie, moviesController.createMovie);
-routes.put('/:id', auth(config), validateMovie, moviesController.updateMovie);
-routes.delete('/:id', auth(config), moviesController.deleteMovie);
+routes.get('/', moviesController.getAll);
+routes.get('/:id', moviesController.getSingle);
+routes.post('/', validateMovie, moviesController.createMovie);
+routes.put('/:id', validateMovie, moviesController.updateMovie);
+routes.delete('/:id', moviesController.deleteMovie);
 
 module.exports = routes;
