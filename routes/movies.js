4a54727a0ebf2e3f77c7fa862ac1b12ff6a6e924
Sterling <sktucker21@byui.edu>
2024-02-26 17:57:28 -0700

    Keep getting 500 error week 08

--- routes/movies.js
+++ routes/movies.js
@@ -1,12 +1,9 @@
 const express = require('express');
-const router = express.Router();
+const routes = express.Router(); // Change variable name from router to routes
 const mongodb = require('../db/connect');
-
 const { ObjectId } = require('mongodb');
-
 const moviesController = require('../controllers/movies');
 const validateMovie = require('../middleware/validate');
-
 const { auth } = require('express-openid-connect');
 
 const config = {
@@ -18,11 +15,10 @@ const config = {
     issuerBaseURL: 'https://dev-aq4g00mxk5l1itvh.us.auth0.com'
 };
 
-router.get('/', auth(config), moviesController.getAll);
-router.get('/:id', auth(config), moviesController.getSingle);
-router.post('/', auth(config), validateMovie, moviesController.createMovie);
-router.put('/:id', auth(config), validateMovie, moviesController.updateMovie);
-router.delete('/:id', auth(config), moviesController.deleteMovie);
+routes.get('/', auth(config), moviesController.getAll);
+routes.get('/:id', auth(config), moviesController.getSingle);
+routes.post('/', auth(config), validateMovie, moviesController.createMovie);
+routes.put('/:id', auth(config), validateMovie, moviesController.updateMovie);
+routes.delete('/:id', auth(config), moviesController.deleteMovie);
 
-
-module.exports = router;
+module.exports = routes;
